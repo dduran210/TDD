16d89d42b15f8ae5fe9a521533983c1bb241e94a
D. Fishback-Duran (Surface) <dduran210@gmail.com>
2020-06-25 11:44:08 -0700

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -11,15 +11,38 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_When_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        # (SETUP) sets up tests
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        # (EXERCISE) calls code to test
+        response = self.client.get('/')
+
+        # (ASSERT) assertions
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
